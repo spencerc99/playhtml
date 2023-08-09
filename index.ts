0d7e25c5519eaea7fec8137b16df7a993cd0beef
Spencer Chang <spencerc99@gmail.com>
2023-08-08 22:32:47 -0700

    try to fix cursor chat on demo site

--- index.ts
+++ index.ts
@@ -1,12 +1,1 @@
 import "./home.scss";
-
-// TODO: convert the guestbook to react and make the hook to make that possible
-function saveName(e: any) {
-  localStorage.setItem("name", JSON.stringify(e.currentTarget.value));
-}
-
-const nameInput = document.getElementsByName("name")[0] as HTMLInputElement;
-nameInput.value = localStorage.getItem("name")
-  ? JSON.parse(localStorage.getItem("name")!)
-  : "";
-nameInput.addEventListener("change", saveName);
