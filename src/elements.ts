4848befe9aa2775e16820f1b85823d736f6b5eef
Spencer Chang <spencerc99@gmail.com>
2023-08-09 19:24:33 -0700

    use chronological instead of reverse chronological sort

--- src/elements.ts
+++ src/elements.ts
@@ -225,7 +225,8 @@ export const TagTypeToElement: Record<
 
         newEntry.innerHTML = `
         <span class="guestbook-entry-timestamp">${dateString}${time}</span><span class="guestbook-entry-name"></span> <span class="guestbook-entry-message"></span>`;
-        guestbookDiv.prepend(newEntry);
+        // TODO: add option to change order?
+        guestbookDiv.appendChild(newEntry);
         newEntry.querySelector(".guestbook-entry-name").innerText = entry.name;
         newEntry.querySelector(".guestbook-entry-message").innerText =
           entry.message;
