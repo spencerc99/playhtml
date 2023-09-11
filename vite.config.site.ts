8befff4c4ba968c0ddc28ca7e081b1d330383ab7
Spencer Chang <spencerc99@gmail.com>
2023-09-11 00:42:24 -0700

    try removing dts from site build

--- vite.config.site.ts
+++ vite.config.site.ts
@@ -1,10 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [dts()],
-
   build: {
     rollupOptions: {
       input: [
