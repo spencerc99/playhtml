87fac22be5d77b0f1aa58b247bc2b628935ebf94
Spencer Chang <spencerc99@gmail.com>
2023-09-11 10:27:19 -0700

    move testing logic to test folder

--- index.tsx
+++ index.tsx
@@ -1,15 +1,3 @@
 import "./home.scss";
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import { Candle, ReactionView } from "./packages/react/example";
-
-ReactDOM.createRoot(
-  document.getElementById("reactContent") as HTMLElement
-).render(
-  <React.StrictMode>
-    <Candle />
-    <ReactionView reaction={{ emoji: "🧡", count: 1 }} />
-  </React.StrictMode>
-);
 
 // TODO: convert the guestbook to react and make the hook to make that possible
