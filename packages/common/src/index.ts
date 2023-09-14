bcb7bff5323ea23b1b5807f5a7fc45737402a75a
Spencer Chang <spencerc99@gmail.com>
2023-09-13 17:24:18 -0700

    move stuff to common

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,5 @@
+import words from "profane-words";
+
 export type ModifierKey = "ctrlKey" | "altKey" | "shiftKey" | "metaKey";
 export const ModifierKeyToName: Record<ModifierKey, string> = {
   ctrlKey: "Control",
@@ -148,3 +150,352 @@ export function getIdForElement(ele: HTMLElement): string | undefined {
 export function getElementFromId(id: string): HTMLElement | null {
   return document.getElementById(id);
 }
+
+const growCursor: string = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='44' height='53' viewport='0 0 100 100' style='fill:black;font-size:26px;'><text y='40%'>🚿</text></svg>")
+      16 0,
+    auto`;
+const cutCursor: string = `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>✂️</text></svg>") 16 0,auto`;
+function canGrowCursorHandler(
+  e: MouseEvent | KeyboardEvent,
+  { getData, getElement, getLocalData, setLocalData }: ElementSetupData
+) {
+  const data = getData();
+  const localData = getLocalData();
+  const element = getElement();
+
+  localData.isHovering = true;
+  if (e.altKey) {
+    if (data.scale <= 0.5) {
+      element.style.cursor = "not-allowed";
+      return;
+    }
+    element.style.cursor = cutCursor;
+  } else {
+    if (data.scale >= data.maxScale) {
+      element.style.cursor = "not-allowed";
+      return;
+    }
+    element.style.cursor = growCursor;
+  }
+
+  setLocalData(localData);
+}
+
+function getClientCoordinates(e: MouseEvent | TouchEvent): {
+  clientX: number;
+  clientY: number;
+} {
+  if ("touches" in e) {
+    const { clientX, clientY } = e.touches[0];
+    return { clientX, clientY };
+  }
+  return { clientX: e.clientX, clientY: e.clientY };
+}
+
+interface FormData {
+  id: string;
+  name: string;
+  message: string;
+  timestamp: number;
+}
+
+export const TagTypeToElement: Record<
+  Exclude<TagType, "can-play">,
+  ElementInitializer
+> = {
+  [TagType.CanMove]: {
+    defaultData: { x: 0, y: 0 },
+    defaultLocalData: { startMouseX: 0, startMouseY: 0 },
+    updateElement: ({ element, data }) => {
+      element.style.transform = `translate(${data.x}px, ${data.y}px)`;
+    },
+    onDragStart: (e: MouseEvent | TouchEvent, { setLocalData }) => {
+      const { clientX, clientY } = getClientCoordinates(e);
+      setLocalData({
+        startMouseX: clientX,
+        startMouseY: clientY,
+      });
+    },
+    onDrag: (
+      e: MouseEvent | TouchEvent,
+      { data, localData, setData, setLocalData, element }
+    ) => {
+      const { clientX, clientY } = getClientCoordinates(e);
+      const { top, left, bottom, right } = element.getBoundingClientRect();
+      if (
+        (right > window.outerWidth && clientX > localData.startMouseX) ||
+        (bottom > window.innerHeight && clientY > localData.startMouseY) ||
+        (left < 0 && clientX < localData.startMouseX) ||
+        (top < 0 && clientY < localData.startMouseY)
+      )
+        return;
+      setData({
+        x: data.x + clientX - localData.startMouseX,
+        y: data.y + clientY - localData.startMouseY,
+      });
+      setLocalData({ startMouseX: clientX, startMouseY: clientY });
+    },
+    resetShortcut: "shiftKey",
+  } as ElementInitializer<MoveData>,
+  [TagType.CanSpin]: {
+    defaultData: { rotation: 0 },
+    defaultLocalData: { startMouseX: 0 },
+    updateElement: ({ element, data }) => {
+      element.style.transform = `rotate(${data.rotation}deg)`;
+    },
+    onDragStart: (e: MouseEvent | TouchEvent, { setLocalData }) => {
+      const { clientX } = getClientCoordinates(e);
+      setLocalData({
+        startMouseX: clientX,
+      });
+    },
+    onDrag: (
+      e: MouseEvent | TouchEvent,
+      { data, localData, setData, setLocalData }
+    ) => {
+      const { clientX } = getClientCoordinates(e);
+      // Calculate distance mouse has moved from the last known position
+      // TODO: scale this according to size
+      let distance = Math.abs(clientX - localData.startMouseX) * 2;
+      let rotation = data.rotation;
+
+      if (clientX > localData.startMouseX) {
+        // Move right
+        rotation += distance; // Change rotation proportional to the distance moved
+      } else if (clientX < localData.startMouseX) {
+        // Move left
+        rotation -= distance; // Change rotation proportional to the distance moved
+      }
+
+      setData({ rotation });
+      setLocalData({ startMouseX: clientX });
+    },
+    resetShortcut: "shiftKey",
+  } as ElementInitializer<SpinData>,
+  [TagType.CanToggle]: {
+    defaultData: false,
+    updateElement: ({ element, data }) => {
+      element.classList.toggle("clicked", data);
+    },
+    onClick: (e: MouseEvent, { data, setData }) => {
+      setData(!data);
+    },
+    resetShortcut: "shiftKey",
+  },
+  [TagType.CanGrow]: {
+    defaultData: { scale: 1 },
+    defaultLocalData: { maxScale: 2, isHovering: false },
+    updateElement: ({ element, data }) => {
+      element.style.transform = `scale(${data.scale})`;
+    },
+    onClick: (e: MouseEvent, { data, element, setData, localData }) => {
+      let { scale } = data;
+      if (e.altKey) {
+        // shrink
+        if (data.scale <= 0.5) {
+          return;
+        }
+
+        scale -= 0.1;
+      } else {
+        // grow
+        element.style.cursor = growCursor;
+        if (data.scale >= localData.maxScale) {
+          return;
+        }
+
+        scale += 0.1;
+      }
+      setData({ ...data, scale });
+    },
+    additionalSetup: (eventData) => {
+      eventData.getElement().addEventListener("mouseenter", (e) => {
+        canGrowCursorHandler(e, eventData);
+        const onKeyDownUp = (e: KeyboardEvent) =>
+          canGrowCursorHandler(e, eventData);
+        document.addEventListener("keydown", onKeyDownUp);
+        document.addEventListener("keyup", onKeyDownUp);
+
+        eventData.getElement().addEventListener("mouseleave", (e) => {
+          document.removeEventListener("keydown", onKeyDownUp);
+          document.removeEventListener("keyup", onKeyDownUp);
+        });
+      });
+    },
+    resetShortcut: "shiftKey",
+  } as ElementInitializer<GrowData>,
+  // TODO: remove this and make it a can-play element. this has too many dependencies to be a part of the core
+  [TagType.CanPost]: {
+    defaultData: [],
+    defaultLocalData: { addedEntries: new Set() },
+    updateElement: ({
+      data: entries,
+      localData: { addedEntries },
+      setLocalData,
+    }) => {
+      const entriesToAdd = entries.filter(
+        (entry) => !addedEntries.has(entry.id)
+      );
+
+      const guestbookDiv = getElementFromId("guestbookMessages")!;
+      entriesToAdd.forEach((entry) => {
+        const newEntry = document.createElement("div");
+        newEntry.classList.add("guestbook-entry");
+        const entryDate = new Date(entry.timestamp);
+        const time = entryDate.toTimeString().split(" ")[0];
+        const isToday = entryDate.toDateString() === new Date().toDateString();
+
+        const dateString = (() => {
+          // TODO: this is naive and incorrect but works most of the time lol
+          const now = new Date();
+          if (
+            now.getFullYear() !== entryDate.getFullYear() ||
+            now.getMonth() !== entryDate.getMonth()
+          ) {
+            return "Sometime before";
+          } else if (isToday) {
+            return "Today";
+          } else if (now.getDate() - entryDate.getDate() === 1) {
+            return "Yesterday";
+          } else if (now.getDate() - entryDate.getDate() < 7) {
+            return "This week";
+          } else {
+            return "Sometime before";
+          }
+        })();
+
+        newEntry.innerHTML = `
+        <span class="guestbook-entry-timestamp">${dateString} at ${time}</span><span class="guestbook-entry-name"></span> <span class="guestbook-entry-message"></span>`;
+        // TODO: add option to change order?
+        guestbookDiv.prepend(newEntry);
+        if (newEntry.querySelector(".guestbook-entry-name")) {
+          // @ts-ignore
+          newEntry.querySelector(".guestbook-entry-name")!.innerText =
+            entry.name;
+        }
+        if (newEntry.querySelector(".guestbook-entry-message")) {
+          // @ts-ignore
+          newEntry.querySelector(".guestbook-entry-message")!.innerText =
+            entry.message;
+        }
+        addedEntries.add(entry.id);
+      });
+
+      setLocalData({ addedEntries });
+    },
+    additionalSetup: ({ getElement, getData, setData }) => {
+      const element = getElement();
+      element.addEventListener("submit", (e: SubmitEvent) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        const entries = getData();
+
+        const formData = new FormData(e.target as HTMLFormElement);
+        // massage formData into new object
+
+        function clearMessage() {
+          const messageEle = element.querySelector('input[name="message"]');
+          if (!messageEle) {
+            return;
+          }
+          // @ts-ignore
+          messageEle.value = "";
+        }
+        // @ts-ignore
+        const inputData = Object.fromEntries(formData.entries());
+
+        if (!inputData.name ?? !inputData.message) {
+          clearMessage();
+          return false;
+        }
+
+        if (
+          words.some((word) => {
+            const regex = new RegExp(`\\b${word}\\b`, "gi");
+            return regex.test(inputData.message) || regex.test(inputData.name);
+          })
+        ) {
+          alert("now why would you try to do something like that?");
+          clearMessage();
+          return false;
+        }
+
+        // TODO: add length validation here
+
+        const timestamp = Date.now();
+        const newEntry: FormData = {
+          name: "someone",
+          message: "something",
+          ...inputData,
+          timestamp,
+          id: `${timestamp}-${inputData.name}`,
+        };
+        setData([...entries, newEntry]);
+        clearMessage();
+        return false;
+      });
+    },
+  } as ElementInitializer<FormData[]>,
+  // TODO: add ability to add max # of duplicates
+  // TODO: add lifespan to automatically prune
+  // TODO: add limit per person / per timeframe.
+  [TagType.CanDuplicate]: {
+    defaultData: [],
+    defaultLocalData: [],
+    updateElement: ({ data, localData, setLocalData, element }) => {
+      const duplicateElementId = element.getAttribute(TagType.CanDuplicate)!;
+      const elementToDuplicate = document.getElementById(duplicateElementId);
+      let lastElement: HTMLElement | null =
+        document.getElementById(localData.slice(-1)?.[0]) ?? null;
+      if (!elementToDuplicate) {
+        console.error(
+          `Element with id ${duplicateElementId} not found. Cannot duplicate.`
+        );
+        return;
+      }
+
+      const canDuplicateTo = element.getAttribute(CanDuplicateTo);
+      function insertDuplicatedElement(newElement: Node) {
+        if (canDuplicateTo) {
+          const duplicateToElement =
+            document.getElementById(canDuplicateTo) ||
+            document.querySelector(canDuplicateTo);
+          if (duplicateToElement) {
+            duplicateToElement.appendChild(newElement);
+            return;
+          }
+        }
+
+        // By default insert after the latest element inserted (or the element to duplicate if none yet)
+        elementToDuplicate!.parentNode!.insertBefore(
+          newElement,
+          (lastElement || elementToDuplicate!).nextSibling
+        );
+      }
+
+      const addedElements = new Set(localData);
+      for (const elementId of data) {
+        if (addedElements.has(elementId)) continue;
+
+        const newElement = elementToDuplicate.cloneNode(true) as HTMLElement;
+        Object.assign(newElement, { ...elementToDuplicate });
+        newElement.id = elementId;
+
+        insertDuplicatedElement(newElement);
+        localData.push(elementId);
+        // TODO: import this to make it work not in browser
+        // @ts-ignore
+        window.playhtml.setupPlayElement(newElement);
+        lastElement = newElement;
+      }
+      setLocalData(localData);
+    },
+    onClick: (_e: MouseEvent, { data, element, setData }) => {
+      const duplicateElementId = element.getAttribute(TagType.CanDuplicate)!;
+      const newElementId =
+        duplicateElementId + "-" + Math.random().toString(36).substr(2, 9);
+
+      setData([...data, newElementId]);
+    },
+  } as ElementInitializer<string[]>,
+};
