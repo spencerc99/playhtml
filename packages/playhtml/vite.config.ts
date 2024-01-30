f11ad4cf2ac82959cd7e7da3ee2e94d69023b9b6
Spencer Chang <spencerc99@gmail.com>
2024-01-30 10:28:20 -0800

    add init file for easy setup

--- packages/playhtml/vite.config.ts
+++ packages/playhtml/vite.config.ts
@@ -5,16 +5,15 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
   plugins: [dts({ rollupTypes: true })],
   build: {
-    // TODO: figure out how to fix this for also building init file
-    // rollupOptions: {
-    //   input: ["src/init.ts", "src/main.ts"],
-    //   output: {
-    //     inlineDynamicImports: false,
-    //   },
-    // },
+    rollupOptions: {
+      input: ["src/init.ts", "src/main.ts"],
+      output: {
+        inlineDynamicImports: false,
+      },
+    },
     lib: {
       entry: path.resolve(__dirname, "src/main.ts"),
-      formats: ["es", "umd"],
+      formats: ["es"],
       name: "playhtml",
       fileName: (format, entryName) => {
         if (entryName === "init") return `init.${format}.js`;
