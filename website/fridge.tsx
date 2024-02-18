ae3d1b6d8b2d2ff91f6bd4cabf2e0df7c66e183b
Spencer Chang <spencerc99@gmail.com>
2024-02-18 17:47:24 -0600

    fridge kinda done

--- website/fridge.tsx
+++ website/fridge.tsx
@@ -1,12 +1,20 @@
-import "./home.scss";
+import "./fridge.scss";
+import words from "profane-words";
 import { TagType } from "@playhtml/common";
 import ReactDOM from "react-dom/client";
 import { withPlay } from "../packages/react/src";
 import React from "react";
 import { PlayProvider } from "../packages/react/src";
 
-interface Props {
+interface FridgeWordType {
+  id?: string;
   word: string;
+  color?: string;
+}
+interface Props extends FridgeWordType {
+  deleteMode?: boolean;
+  onDeleteWord?: () => void;
+  className?: string;
 }
 
 const FridgeWord = withPlay<Props>()(
@@ -14,10 +22,53 @@ const FridgeWord = withPlay<Props>()(
     tagInfo: [TagType.CanMove],
   },
   ({ props }) => {
-    const { word } = props;
+    const { id, word, deleteMode, onDeleteWord, className } = props;
     return (
-      <div selector-id="#fridge .fridgeWordHolder" className="fridgeWordHolder">
-        <div className="fridgeWord">{word}</div>
+      <div
+        id={id}
+        selector-id="#fridge .fridgeWordHolder"
+        className="fridgeWordHolder"
+      >
+        <div
+          className={`fridgeWord ${className}`}
+          style={{
+            "--word-color": props.color,
+            ...(deleteMode
+              ? {
+                  border: "2px dotted red",
+                }
+              : {}),
+          }}
+        >
+          {word}
+          {deleteMode ? (
+            <button
+              style={{
+                background: "red",
+                borderRadius: "50%",
+                width: "1.5em",
+                marginLeft: "4px",
+                color: "white",
+                position: "absolute",
+                top: "-8px",
+                right: "-12px",
+                height: "1.5em",
+                minHeight: "unset",
+                lineHeight: "0",
+              }}
+              onClick={() => {
+                if (!deleteMode) return;
+
+                onDeleteWord?.();
+              }}
+            >
+              {" "}
+              x
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     );
   }
@@ -72,10 +123,136 @@ const Words = [
   "always",
 ];
 
+const WordControls = withPlay()(
+  {
+    defaultData: [] as FridgeWordType[],
+    id: "newWords",
+  },
+  ({ data, setData }) => {
+    const [input, setInput] = React.useState("");
+    const [deleteMode, setDeleteMode] = React.useState(false);
+    const userColor =
+      window.cursors?.color || localStorage.getItem("userColor") || undefined;
+
+    function clearMessage() {
+      setInput("");
+    }
+
+    function onSubmit() {
+      if (!input) {
+        return;
+      }
+      if (
+        words.some((word) => {
+          const regex = new RegExp(`\\b${word}\\b`, "gi");
+          return regex.test(input);
+        })
+      ) {
+        alert(
+          "we don't seem to like that word :( please keep things nice here"
+        );
+        clearMessage();
+        return false;
+      }
+
+      setData([
+        ...data,
+        { word: input, color: userColor, id: Date.now().toString() },
+      ]);
+      clearMessage();
+    }
+
+    return (
+      <>
+        {data.map(({ word, color, id }) => (
+          <FridgeWord
+            id={id}
+            key={id}
+            word={word}
+            color={color}
+            deleteMode={deleteMode}
+            className="custom"
+            onDeleteWord={() => {
+              const idxToDelete = data.findIndex((w) => {
+                if (id) {
+                  return id === w.id;
+                }
+
+                return word === w.word && color === w.color;
+              });
+              console.log(
+                "found idx",
+                idxToDelete,
+                id,
+                word,
+                color,
+                data[idxToDelete]
+              );
+              setData(data.filter((_, idx) => idx !== idxToDelete));
+              console.log(data);
+            }}
+          />
+        ))}
+        <div
+          style={{
+            position: "fixed",
+            bottom: "20%",
+            right: "20%",
+            display: "flex",
+            gap: ".5em",
+          }}
+        >
+          <input
+            placeholder="New word..."
+            value={input}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") onSubmit();
+            }}
+            onChange={(e) => setInput(e.target.value.trim())}
+          ></input>
+          <button
+            onClick={onSubmit}
+            disabled={!Boolean(input)}
+            style={{
+              padding: ".5em 1em",
+            }}
+          >
+            Add
+          </button>
+        </div>
+        <div
+          style={{
+            position: "fixed",
+            bottom: "16%",
+            right: "20%",
+            display: "flex",
+            gap: ".5em",
+          }}
+        >
+          <button
+            onClick={() => setDeleteMode(!deleteMode)}
+            style={{
+              background: !deleteMode ? "red" : undefined,
+              padding: ".5em 1em",
+            }}
+          >
+            {deleteMode ? "Stop" : "Delete"}
+          </button>
+        </div>
+      </>
+    );
+  }
+);
+
 ReactDOM.createRoot(document.getElementById("fridge") as HTMLElement).render(
-  <PlayProvider>
+  <PlayProvider
+    initOptions={{
+      room: window.location.pathname,
+    }}
+  >
     {Words.map((w, i) => (
       <FridgeWord key={i} word={w} />
     ))}
+    <WordControls />
   </PlayProvider>
 );
