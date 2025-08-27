67465fd6d855ae5094a3aebdf4e9917d6cb11c79
Spencer Chang <spencerc99@gmail.com>
2025-08-27 15:57:07 -0700

    remove indexeddb

--- packages/playhtml/src/index.ts
+++ packages/playhtml/src/index.ts
@@ -1,7 +1,6 @@
 /// <reference lib="dom"/>
 /// <reference types="vite/client" />
 import YPartyKitProvider from "y-partykit/provider";
-import { IndexeddbPersistence } from "y-indexeddb";
 import "./style.scss";
 import {
   ElementData,
@@ -513,10 +512,6 @@ async function initPlayHTML({
 
     cursorClient = new CursorClientAwareness(yprovider, cursorOptions);
   }
-
-  // @ts-ignore
-  // TODO: we should backup in indexeddb too but not using this bc it introduces a bunch of weird conflicts
-  const _indexedDBProvider = new IndexeddbPersistence(room, doc);
 
   if (extraCapabilities) {
     for (const [tag, tagInfo] of Object.entries(extraCapabilities)) {
